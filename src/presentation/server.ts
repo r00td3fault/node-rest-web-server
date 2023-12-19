7cc882e91062d86ca869a4fe8108812a57ab6f25
rhurtado <r00td3fault@gmail.com>
2023-12-19 17:40:38 -0500

    Start Rest/web server with typescript

---
+++ src/presentation/server.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import path from 'path';
+
+interface Options {
+    port: number;
+    publicPath?: string;
+}
+
+export class Server {
+
+    private app = express();
+    private readonly port: number;
+    private readonly publicPath: string;
+
+    constructor(options: Options) {
+        const { port, publicPath } = options;
+
+        this.port = port;
+        this.publicPath = publicPath!;
+    }
+
+
+    async start() {
+
+        //* Middlewares
+
+        //* Public folder
+        this.app.use(express.static(this.publicPath));
+
+
+        this.app.get('*', (req, res) => {
+            const indexPath = path.join(__dirname, `../../${this.publicPath}/index.html`);
+            res.sendFile(indexPath);
+        });
+
+
+
+        this.app.listen(this.port, () => {
+            console.log(`Server running on port ${this.port}`);
+        })
+
+    }
+
+
+}
